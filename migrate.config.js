409b37fe1956eed3aeacce905fd10bf174d1dc97
Ron W. van Etten <rondlite@gmail.com>
2021-02-15 19:13:22 +0100

    prepare for 3.11.1

---
+++ migrate.config.js
@@ -0,0 +1,14 @@
+export default {
+  tracks: [
+    {
+      namespace: "accounts",
+      package: "@reactioncommerce/api-plugin-accounts",
+      version: 3
+    },
+    {
+      namespace: "simple-authorization",
+      package: "@reactioncommerce/api-plugin-authorization-simple",
+      version: 5
+    }
+  ]
+};
